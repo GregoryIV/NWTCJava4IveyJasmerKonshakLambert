9930293bb0e32a1ad16fd407557075e75ec3c263
GregoryIV <93711238+GregoryIV@users.noreply.github.com>
2022-10-28 16:35:08 -0500

    Have one member perform an intial commit (push).
    7. Work with your team to develop an initial Exception handling structure (try-catch-final blocks) in your code, commit this to the repository (10/10 Points)

---
+++ src/LoggingStructure.java
@@ -0,0 +1,16 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LoggingStructure {
+
+    // Instantiate the logger object
+    final static Logger log = LogManager.getLogger(LoggingStructure.class.getName());
+
+    public static void main(String[] args) {
+
+        log.debug("Hello world! This is a debug message");
+        log.info("Hello world! This is a info message");
+
+    }
+
+}
